d5199c725a39c968eb17724673291200401e4ee8
guhenf <guhenf@gmail.com>
2024-10-15 20:55:52 -0300

    init sequelize

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import petsRouter from "./routes/petsRoutes";
+import sequelize from "./config/database";
 
 const app = express();
 const port = 3000;
 
+app.use(json())
 app.use(petsRouter)
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+sequelize
+  .sync({force: true})
+  .then(() => {
+    console.log("Database")
+    app.listen(port, () => console.log("Listening"))
+  })
